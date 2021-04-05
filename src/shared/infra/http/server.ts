cd746f28de00ff58228dcd3978b245197da20f73
brunoFgR <brunofds281@gmail.com>
2021-04-05 18:49:44 -0300

    style: arranging documents with eslint

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -8,21 +8,21 @@ const app = express();
 app.use(express.json());
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
-  if (err instanceof AppErrors) {
-    return response.status(err.statusCode).json({
-      status: 'error',
-      message: err.message,
+    if (err instanceof AppErrors) {
+        return response.status(err.statusCode).json({
+            status: 'error',
+            message: err.message,
+        });
+    }
+
+    console.error(err);
+
+    return response.status(500).json({
+        status: 'error',
+        message: 'Internal Server Error',
     });
-  }
-
-  console.error(err);
-
-  return response.status(500).json({
-    status: 'error',
-    message: 'Internal Server Error',
-  });
 });
 
 app.listen(3000, () => {
-  console.log('Server running port 3000');
+    console.log('Server running port 3000');
 });
