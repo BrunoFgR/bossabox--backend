cbaad3aacc1fcdf30d987a44e17455a732e89f30
brunoFgR <brunofds281@gmail.com>
2021-04-05 18:34:38 -0300

    feat(raiz): creating project architecture

---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,28 @@
+import 'reflect-metadata';
+
+import express, { Request, Response, NextFunction } from 'express';
+import AppErrors from '@shared/errors/AppErrors';
+
+const app = express();
+
+app.use(express.json());
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppErrors) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error',
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server running port 3000');
+});
